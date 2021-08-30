d4f625a1766c64ffe1f387ae18404abf6a523eb7
XCY <personalXCY@outlook.com>
2021-08-30 20:33:39 +0800

    use yaml parameter file to initialize environment

--- swarm_simulator/app/main.cpp
+++ swarm_simulator/app/main.cpp
@@ -1,9 +1,69 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "rclcpp/rclcpp.hpp"
 #include "../plugins/plugins"
 #include "../core/core"
+
+namespace simulator
+{
+    class LauncherNode : public rclcpp::Node
+    {
+    public:
+        LauncherNode() : rclcpp::Node{"simulator_mainapp"}
+        {
+            using namespace std;
+
+            this->declare_parameter("map");
+            this->declare_parameter("names");
+            this->declare_parameter("xs");
+            this->declare_parameter("ys");
+            this->declare_parameter("zs");
+            this->declare_parameter("range", 100.0);
+            this->declare_parameter("slam_freq", 5.0);
+
+            auto map_file = this->get_parameter("map").as_string();
+            auto names = this->get_parameter("names").as_string_array();
+            auto xs = this->get_parameter("xs").as_double_array();
+            auto ys = this->get_parameter("ys").as_double_array();
+            auto zs = this->get_parameter("zs").as_double_array();
+            auto range = this->get_parameter("range").as_double();
+            auto slam_freq = this->get_parameter("slam_freq").as_double();
+
+            int num = min({names.size(), xs.size(), ys.size(), zs.size()});
+
+            vector<string> robot_names{names.begin(), names.begin() + num};
+            vector<tuple<double, double, double>> robot_poses;
+            for (int i = 0; i < num; i++)
+                robot_poses.push_back({xs[i], ys[i], zs[i]});
+
+            this->core_ptr->initSimulationEnv(robot_names, robot_poses, map_file);
+
+            for (int i = 0; i < num; i++)
+            {
+                this->fov_list.push_back(std::make_shared<plugin::FieldOfView>(robot_names[i], slam_freq, range, this->core_ptr));
+                this->nav_list.push_back(std::make_shared<plugin::NavigationPlugin>(robot_names[i], this->core_ptr));
+            }
+        }
+
+        std::vector<rclcpp::Node::SharedPtr> GetNodes(void)
+        {
+            std::vector<rclcpp::Node::SharedPtr> nodes;
+            nodes.push_back(this->core_ptr);
+            for (auto &&fov : this->fov_list)
+                nodes.push_back(fov);
+            for (auto &&nav : this->nav_list)
+                nodes.push_back(nav);
+            return nodes;
+        }
+
+    private:
+        std::shared_ptr<core::CoreNode> core_ptr = std::make_shared<core::CoreNode>();
+        std::vector<std::shared_ptr<plugin::FieldOfView>> fov_list;
+        std::vector<std::shared_ptr<plugin::NavigationPlugin>> nav_list;
+    };
+} // namespace simulator
 
 int main(int argc, char const *argv[])
 {
@@ -12,53 +72,12 @@ int main(int argc, char const *argv[])
 
     rclcpp::init(argc, argv);
 
-    std::string map_file; 
-    if(argc > 1)
-    {  
-        map_file = argv[1];
-    }
-    if(map_file.size() == 0)
-        map_file = "/home/wei/swarm_2d_ws/src/swarm_simulator/app/warehouse_01.png";
-    int robot_num = 10;
-    vector<string> robot_names;
-    vector<tuple<double, double, double>> robot_init_poses;
+    auto launcher_ptr = std::make_shared<LauncherNode>();
 
-    double x_start = 2.0;
-    double y_start = 3.0;
-    double yaw_start = 0.0; 
-    for(int i = 0; i < robot_num/2; i++){
-        robot_names.push_back("tb"+to_string(i));
-        robot_init_poses.push_back(make_tuple(x_start, y_start, yaw_start));
-        x_start += 1.0;
-    }
-    y_start += 4.0;
-    x_start = 2.0;
-    for(int i = robot_num/2; i < robot_num; i++){
-        robot_names.push_back("tb"+to_string(i));
-        robot_init_poses.push_back(make_tuple(x_start, y_start, yaw_start));
-        x_start += 1.0;
-    }
-
-    
-
-    shared_ptr<core::CoreNode> core_ptr = make_shared<core::CoreNode>();
-    core_ptr->initSimulationEnv(robot_names, robot_init_poses, map_file);
-
-    vector<shared_ptr<plugin::FieldOfView>> fov_list;
-    vector<shared_ptr<plugin::NavigationPlugin>> nav_list;
-    for(int i = 0; i < robot_num; i++)
-    {
-        fov_list.push_back(make_shared<plugin::FieldOfView>(robot_names[i], 5, 100, core_ptr));
-        nav_list.push_back(make_shared<plugin::NavigationPlugin>(robot_names[i], core_ptr));
-    }
-    
     rclcpp::executors::MultiThreadedExecutor executor;
-    executor.add_node(core_ptr);
-    for(int i = 0; i < robot_num; i++)
-    {
-        executor.add_node(fov_list[i]);
-        executor.add_node(nav_list[i]);
-    }
+
+    for (auto &&node_ptr : launcher_ptr->GetNodes())
+        executor.add_node(node_ptr);
 
     executor.spin();
 
